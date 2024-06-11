a05015660c1f053396609ba6481fba7628f8299f
Alex Xu <a3824036@126.com>
2024-06-11 20:14:59 +0800

    update

--- src/services/quilibrium.service.ts
+++ src/services/quilibrium.service.ts
@@ -8,11 +8,48 @@ import axios from 'axios'
 
 const namespace = 'quilibrium_service'
 
-export async function getQuilibriumClient(): Promise<boolean> {
+interface QuilibriumPM2Client {
+  pmExecPath: string
+  pmCwd: string
+  user: string
+  raw: any
+}
+
+const filenameReg = /node-(\d+\.\d+\.\d+)-(darwin|linux)-(arm64|amd64)/i
+
+export async function getQuilibriumClient(): Promise<QuilibriumPM2Client | null> {
   const utils = getUtils()
   const result = await utils.shellScripts.scriptRunner.exec('pm2 jlist')
-  console.log(result)
-  return true
+
+  if (!result.success) {
+    console.log('pm2 jlist failed')
+    return null
+  }
+  try {
+    const pm2processes = JSON.parse(result.stdout.trim())
+    const client = pm2processes.find((p: any) => p.name === 'ceremonyclient')
+    if (!client) {
+      return null
+    }
+    if (client.pm2_env.status !== 'online') return null
+    // /Users/ada1/ceremonyclient/node/node-1.4.19-darwin-arm64
+    const path = client.pm2_env.pm_exec_path as string
+    const filename = path.substring(path.lastIndexOf('/') + 1)
+    if (!path) return null
+
+    if (!filenameReg.test(filename)) return null
+    const ret: QuilibriumPM2Client = {
+      pmExecPath: path,
+      pmCwd: client.pm2_env.pm_cwd,
+      user: client.pm2_env.USER,
+      raw: client,
+    }
+
+    return ret
+  } catch (error) {
+    console.log('pm2 jlist parse failed: ', (error as Error).message)
+  }
+  return null
 }
 
 const QuilibriumService = {
