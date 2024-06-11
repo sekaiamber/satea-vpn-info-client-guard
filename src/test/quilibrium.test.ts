0e46afd9cbe6768fb532f12aa9a1d01011023305
Alex Xu <a3824036@126.com>
2024-06-11 20:15:15 +0800

    update

--- src/test/quilibrium.test.ts
+++ src/test/quilibrium.test.ts
@@ -2,6 +2,7 @@ import QuilibriumService from '../services/quilibrium.service'
 
 async function task(): Promise<void> {
   const client = await QuilibriumService.getQuilibriumClient()
+  console.log(client)
 }
 
 task().catch((e) => console.log(e))
